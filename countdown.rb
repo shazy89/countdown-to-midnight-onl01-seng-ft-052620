4e54178f3c91571d1e0ccb2e7af7029395d47efc
Ed Shaziman <erdogan_oh@hotmail.com>
2020-05-27 17:23:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,11 @@
 #write your code here
-def countdown (num) 
-  while num > 0 
-   puts "#{num} SECOND(S)"
-   num -= 1
+def countdown(number)
+  while number > 0 
+    puts "#{number} SECOND(S)!"
+    number -= 1
   end
-   "HAPPY NEW YEAR!"
-  end
-  
+  "HAPPY NEW YEAR!"
+end
 
 def countdown_with_sleep(num)
   while num > 0 
